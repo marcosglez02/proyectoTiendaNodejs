ff3a05b7354736bbff7883007cbd67edb991f423
Manuel Alexis Flores Prado <alexisprfl@gmail.com>
2023-03-21 17:14:24 -0700

    Actualizacion Encabezado

--- router/index.js
+++ router/index.js
@@ -23,17 +23,143 @@ router.get('/admin/ventas',(req,res)=>{
         res.render('admin/ventas.html',valores)
 });
 
-router.get('/carrito.html',(req,res)=>{
+router.get('/carrito',(req,res)=>{
    const valores={
     titulo:"Carrito"
    }
     res.render('carrito.html',valores)
 });
 
-router.post('/carrito.html',(req,res)=>{
+router.post('/carrito',(req,res)=>{
     const valores={
         titulo:"Carrito"
        }
         res.render('carrito.html',valores)
 });
+
+
+
+router.get('/productos',(req,res)=>{
+    const valores={
+     titulo:"productos"
+    }
+     res.render('productos.html',valores)
+ });
+ 
+ router.post('/productos',(req,res)=>{
+     const valores={
+         titulo:"productos"
+        }
+         res.render('productos.html',valores)
+ });
+
+
+ router.get('/admin',(req,res)=>{
+    const valores={
+     titulo:"admin"
+    }
+     res.render('admin.html',valores)
+ });
+ 
+ router.post('/admin',(req,res)=>{
+     const valores={
+         titulo:"admin"
+        }
+         res.render('admin.html',valores)
+ });
+
+
+ router.get('/contactos',(req,res)=>{
+    const valores={
+     titulo:"contactos"
+    }
+     res.render('contactos.html',valores)
+ });
+ 
+ router.post('/contactos',(req,res)=>{
+     const valores={
+         titulo:"contactos"
+        }
+         res.render('contactos.html',valores)
+ });
+
+
+ router.get('/login',(req,res)=>{
+    const valores={
+     titulo:"login"
+    }
+     res.render('login.html',valores)
+ });
+ 
+ router.post('/login',(req,res)=>{
+     const valores={
+         titulo:"login"
+        }
+         res.render('login.html',valores)
+ });
+
+
+
+
+ router.get('/Nosotros',(req,res)=>{
+    const valores={
+     titulo:"Nosotros"
+    }
+     res.render('Nosotros.html',valores)
+ });
+ 
+ router.post('/Nosotros',(req,res)=>{
+     const valores={
+         titulo:"Nosotros"
+        }
+         res.render('Nosotros.html',valores)
+ });
+
+
+ router.get('/pago',(req,res)=>{
+    const valores={
+     titulo:"pago"
+    }
+     res.render('pago.html',valores)
+ });
+ 
+ router.post('/pago',(req,res)=>{
+     const valores={
+         titulo:"pago"
+        }
+         res.render('pago.html',valores)
+ });
+
+ router.get('/registro',(req,res)=>{
+    const valores={
+     titulo:"registro",
+     encabezado:"registrate"
+    }
+     res.render('registro.html',valores)
+ });
+ 
+ router.post('/registro',(req,res)=>{
+     const valores={
+         titulo:"registro",
+         encabezado:"registrate"
+        }
+         res.render('registro.html',valores)
+ });
+
+
+ router.get('/recu',(req,res)=>{
+    const valores={
+     titulo:"recu",
+     encabezado:"recu"
+    }
+     res.render('recu.html',valores)
+ });
+ 
+ router.post('/recu',(req,res)=>{
+     const valores={
+         titulo:"recu",
+         encabezado:"recu"
+        }
+         res.render('recu.html',valores)
+ });
 export default router;
