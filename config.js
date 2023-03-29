de71ce78f271c77b0b9f392dbe39687075a5d1b4
Marcos Gonzalez <marcos.abraham2002@gmail.com>
2023-03-28 21:31:49 -0700

    Arreglo de la conexión al servidor

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ export const CLOUDINARY_API_SECRET = process.env.CLOUD_KEY_SECRET;
 // CREDENCIALES DE LA BASE DE DATOS
 export const DB_HOST = process.env.DB_HOST;
 export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASS;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_PORT = process.env.DB_PORT;
 export const DB_NAME = process.env.DB_NAME;
 
