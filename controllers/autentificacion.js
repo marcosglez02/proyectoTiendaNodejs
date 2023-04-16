c8b01401a0ceb676cdc86120974b0a6c3fbe35aa
IsraChido <2020030223@upsin.edu.mx>
2023-04-16 12:26:19 -0700

    autentificacion.js pruebas de error

--- controllers/autentificacion.js
+++ controllers/autentificacion.js
@@ -25,27 +25,65 @@ export const validarCredenciales = async (req, res) => {
 
 export const registroUsuarios = async(req,res)=>{
   try{
-    if(req.body.contraseñaUsuario != req.body.contraConfirmar){
-      console.log("Contraseñas diferentes!!!")
-      return res.redirect('/registro')
+
+    const nuevoUsuario = {
+      idUsuario: req.insertidUsuario,
+      usuario: req.body.usuario,
+      nombreUsuario: req.body.nombreUsuario,
+      apellidoUsuario: req.body.apellidoUsuario,
+      domicilioUsuario: req.body.domicilioUsuario,
+      telefonoUsuario: req.body.telefonoUsuario,
+      correoUsuario: req.body.correoUsuario,
+      contraseñaUsuario: req.body.contraseñaUsuario,
+      estado: 1
+    }
+
+    // Cambiar returns a un limpiar, y añadirles un alert para el usuario
+
+    if(req.body.usuario == 0){
+      return res.redirect('/registro'),
+      console.log("Ingrese un usuario, no puede estar vacío.")
     } 
 
-  const nuevoUsuario = {
-    idUsuario: req.insertidUsuario,
-    usuario: req.body.usuario,
-    nombreUsuario: req.body.nombreUsuario,
-    apellidoUsuario: req.body.apellidoUsuario,
-    domicilioUsuario: req.body.domicilioUsuario,
-    telefonoUsuario: req.body.telefonoUsuario,
-    correoUsuario: req.body.correoUsuario,
-    contraseñaUsuario: req.body.contraseñaUsuario,
-    estado: 1
-  }
+    if(req.body.nombreUsuario == 0){
+      return res.redirect('/registro'),
+      console.log("Ingrese su nombre, no puede estar vacío.")
+    } 
+
+    if(req.body.apellidoUsuario == 0){
+      return res.redirect('/registro'),
+      console.log("Ingrese su apellido, no puede estar vacío.")
+    } 
+
+    if(req.body.contraseñaUsuario != req.body.contraConfirmar){
+      return res.redirect('/registro'),
+      console.log("Contraseñas diferentes.")
+    } 
+
+    if(req.body.contraseñaUsuario.length < 5){
+      return res.redirect('/registro'),
+      console.log("La contraseña no puede ser menor de 5 carácteres.")
+    } 
+
+    if(req.body.contraseñaUsuario.length > 20){
+      return res.redirect('/registro'),
+      console.log("La contraseña no puede ser mayor a 20 carácteres.")
+    } 
+
+    if(req.body.telefonoUsuario.length < 10){
+      return res.redirect('/registro'),
+      console.log("El teléfono no puede ser menor de 10 dígitos (tiene que ser de 10).")
+    } 
+    
+    if(req.body.telefonoUsuario.length > 10){
+      return res.redirect('/registro'),
+      console.log("El teléfono no puede ser mayor de 10 dígitos (tiene que ser de 10).")
+    } 
 
   await pool.query("insert into usuarios set ?", [nuevoUsuario])
   res.redirect('/')
   }catch(error){
-    res.status(400).send("Secedió un error")
+    res.status(400).send("Sucedió un error")
   }
 }
 
